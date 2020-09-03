779928bd85c3e76524a9d28ec552aa10c25a32e9
njuhgao <57426572+njuhgao@users.noreply.github.com>
2020-09-03 14:34:13 +0800

    turntable 1&2 ctrl supported

--- IRM_TurnTable_Example.m
+++ IRM_TurnTable_Example.m
@@ -4,13 +4,25 @@ count = 200;
 % Motorprepare(Room); 
 % pause;
 use_TurnTable = 1;
+TurnTable_No = 2;
 %%
 Data = cell(count,1);
 
-%% TurnTable Prepare  
+%% Turntable connect
 if(use_TurnTable)
-    u = udp('192.168.1.177',8888); 
-    fopen(u);
+    if TurnTable_No == 1        
+        u = udp('192.168.1.177',8888);
+        fopen(u);
+    elseif TurnTable_No == 2
+        dAngle = 1.8;
+        angle = dAngle*count;
+        tcpclient = tcpip('192.168.181.181',8181, 'Timeout', 60,'OutputBufferSize',10240,'InputBufferSize',10240);
+        fopen(tcpclient);
+        fwrite(tcpclient,'CT+SPKMUTE(1);');
+        fwrite(tcpclient,'CT+HEARTBEAT(0);');
+    else
+        error('No such TurnTable_No.');
+    end
 end
 %% Measurement Go
 % pause(10);
@@ -33,25 +45,23 @@ for ii = 1:(count)
 
     %% Turntable Op.
     if(use_TurnTable)
-        fwrite (u,'1');
-        flag = 1;
-        while flag
-            Check = 'none';
-            Check = fscanf(u,'%s');
-            if (strcmp(Check,'done'))
-                flag = 0;
+        if TurnTable_No == 1
+            pause(0.5);
+            fwrite (u,'1');
+            flag = 1;
+            while flag
+                Check = 'none';
+                Check = fscanf(u,'%s');
+                if (strcmp(Check,'done'))
+                    flag = 0;
+                end
             end
+        elseif TurnTable_No == 2
+            pause(0.1);
+            turnTable(tcpclient, dAngle);            
+        else
+            error(' No such TurnTable_No.');
         end
-%         pause(0.5)
-%         fwrite (u,'1');
-%         flag = 1;
-%         while flag
-%             Check = 'none';
-%             Check = fscanf(u,'%s');
-%             if (strcmp(Check,'done'))
-%                 flag = 0;
-%             end
-%         end
     end
     %% Fetch Result
     if config.usingParallel
@@ -79,5 +89,14 @@ filename = string(datetime,'yyyyMMdd_HH_mm_ss');
 save(filename,'Data');
 %% TurnTable
 if(use_TurnTable)
-    fclose(u);
+    if TurnTable_No == 1        
+        fclose(u);
+    elseif TurnTable_No == 2
+        pause(0.5);
+        turnTable(tcpclient, -angle);
+        fclose(tcpclient);
+        delete(tcpclient);
+    else
+        error(' No such TurnTable_No.');
+    end
 end
---
+++ turnTable.m
@@ -0,0 +1,35 @@
+function [CR] = turnTable(tcpclient, angle)
+%TURNTABLE 
+if angle>0
+    turn = 0;
+elseif angle<0
+    turn = 1;
+    angle = -angle;
+else
+    error('angle');
+end
+
+fwrite(tcpclient,['CT+TRUNSINGLE(',num2str(turn),',',num2str(angle),');']);
+idx = 0;
+while(1) %Polling untile data received
+    nBytes = get(tcpclient,'BytesAvailable');
+    if nBytes>0
+        receive = fread(tcpclient,nBytes);% read data from tcp server
+        data=char(receive'); % tranform ASCII 2 str
+        if contains(data,'CR+OK;')
+            CR = 'CR+OK;';
+        elseif contains(data,'CR+EVENT=TB_PAUSE;')
+            CR = 'CR+EVENT=TB_PAUSE;';
+            break;
+%         elseif strcmp(data,'#')            
+        else
+            error(data);
+        end
+    end
+    pause(0.1);
+    idx = idx+1;
+    assert(  idx < 100, ' Turntable time out! ' );
+end
+CR = 0;
+end
+
